979772968e5f6bcac4ebb6b6f9e3dd31deb2a9de
dhananjayboragari <dhananjayboragari@gmail.com>
2026-06-28 11:59:41 +0530

    Update dashboard_online.js

--- dashboard_online.js
+++ dashboard_online.js
@@ -1,30 +1,39 @@
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getDatabase, ref, set, onDisconnect, serverTimestamp } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 import { getAuth, onAuthStateChanged } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const db = getDatabase();
-const auth = getAuth();
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
   if (!user) return;
 
   const userRef = ref(db, "status/" + user.uid);
 
-  // online
   set(userRef, {
     state: "online",
     lastSeen: serverTimestamp()
   });
 
-  // disconnect hole
   onDisconnect(userRef).set({
     state: "offline",
     lastSeen: serverTimestamp()
   });
 
-  // continuous update
   setInterval(() => {
     set(userRef, {
       state: "online",
